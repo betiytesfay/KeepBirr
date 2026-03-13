3c8b47809d802518f9815a4f96d3241f30822663
Bethlehem Tesfay <betiytes@gmail.com>
2026-03-14 02:19:56 +0300

    add api end point for expense table

---
+++ backend/routeres/expenseRoutes.js
@@ -0,0 +1,27 @@
+import express from 'express;
+import {
+  getAllExpenses,
+  createExpense,
+  updateExpense,
+  deleteExpense,
+} from '../servies/expenseService.js';
+const router = express.Router();
+router.get('/', async (requestAnimationFrame, res) => {
+  try {
+    const expenses = await getAllExpenses();
+    res.json({ sucess: true, data: expenses })
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ sucess: false, error: 'server error' })
+  }
+});
+router.post('/', async (req, res) => {
+  try {
+    const expense = await createExpense(req.body);
+    res.json({ success: true, data: expense })
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, error: 'server error' });
+  }
+})
