cd1dcdbe5e8ea7f45d323ae2dd4c5d838d087afa
Bethlehem Tesfay <betiytes@gmail.com>
2026-05-18 02:16:33 +0300

    feat(server): add Express server with auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,35 @@
-const express = require("express")
-const cors = require("cors")
-const pool = require("./data/db")
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.get("/", async (req, res) => {
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { db } from './db/index.js';
+import { users } from './db/schema.js';
+import authRoutes from "./routeres/authRouter.js"
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+// Test route
+app.get('/api/users', async (req, res) => {
   try {
-    const result = await pool.query("Select Now()")
-    res.send(`Time :${result.rows[0].now}`)
+    const allUsers = await db.select().from(users);
+    res.json({ success: true, data: allUsers });
+  } catch (error) {
+    console.error('Detailed error:', error);  // ← add this
+    res.status(500).json({ success: false, error: error.message }); // ← send message
   }
-  catch (err) {
-    console.error(err);
-    res.status(500).send("server error")
-  }
-})
-app.listen(5000, () => {
-  console.log("server is running on port 5000")
-})
+});
+
+app.get('/', (req, res) => {
+  res.send('Expense Tracker API is running');
+});
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught exception:', err);
+});
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled rejection:', err);
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
